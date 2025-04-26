608cc20ba250573b1b298bac26f03f1b9ae32a89
Kolitha Rathnayake <kolitharathnayake229@gmail.com>
2025-04-26 07:54:05 +0530

    update DAOFactory,crudDAO,Types

---
+++ Serenity-Mental-Health-Therapy-Center/src/main/java/lk/ijse/gdse71/Courswork/repostory/custom/DAOFactory.java
@@ -0,0 +1,30 @@
+package lk.ijse.gdse71.Courswork.repostory.custom;
+
+import lk.ijse.project.mentalHealthTherapyCenter.repostory.custom.impl.*;
+
+public class DAOFactory {
+    public static DAOFactory daoFactory;
+    private DAOFactory() {}
+
+    public static DAOFactory getInstance() {
+        if (daoFactory == null) {
+            daoFactory = new DAOFactory();
+        }
+        return daoFactory;
+    }
+    @SuppressWarnings("unchecked")
+    public <T extends SuperDAO>T getDAO(DAOType daoType) {
+        return switch (daoType) {
+            case APPOINTMENTS -> (T) new AppointmentDAOImpl();
+            case USER ->(T) new UserDAOImpl();
+            case PATIENT -> (T) new PatientDAOImpl();
+            case PAYMENT -> (T) new PaymentDAOImpl();
+            case THERAPIST -> (T) new TherapistDAOImpl();
+            case THERAPY_PROGRAMS -> (T) new TProgramDAOImpl();
+            case QUERY -> (T) new QueryDAOImpl();
+            case PROGRAM_DETAILS -> (T) new ProgramDetailsDAOImpl();
+            default -> null;
+        };
+    }
+}
+
---
+++ Serenity-Mental-Health-Therapy-Center/src/main/java/lk/ijse/gdse71/Courswork/repostory/custom/DAOType.java
@@ -0,0 +1,5 @@
+package lk.ijse.gdse71.Courswork.repostory.custom;
+
+public enum DAOType {
+    APPOINTMENTS ,USER,PATIENT,PAYMENT,THERAPIST,THERAPY_PROGRAMS,QUERY,PROGRAM_DETAILS
+}
